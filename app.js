b5bc1ef811129b8ee115b6accc098dd49c6cb6d4
Charles Painter <cpainter85@gmail.com>
2015-07-28 14:39:32 -0400

    add new comments

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.config([
 app.factory('posts', [function (){
   var o = {
     posts: [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
+            {title: 'post 1', upvotes: 5, comments: []},
+            {title: 'post 2', upvotes: 2, comments: []},
+            {title: 'post 3', upvotes: 15, comments: []},
+            {title: 'post 4', upvotes: 9, comments: []},
+            {title: 'post 5', upvotes: 4, comments: []}
             ]
   };
   return o;
@@ -59,6 +59,15 @@ app.controller('PostsCtrl', [
     $scope.post = posts.posts[$stateParams.id];
     $scope.incrementUpvotes = function (comment) {
       comment.upvotes ++;
+    };
+    $scope.addComment = function () {
+      if($scope.body === '') return;
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
     };
   }
 ]);
